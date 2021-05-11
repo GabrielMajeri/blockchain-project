80ad31595bf6a6342573de8a572ebf59d5d91ab3
Bogdan Nitica <bogdan.nitica99@gmail.com>
2021-05-11 23:30:11 +0300

    configure truffle

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import getWeb3 from "./getWeb3";
+import ProjectsContract from "./contracts/Projects.json";
 
 import Routes from "./Routes";
 
 function App() {
   const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
     // Connect to the local Ethereum node
@@ -13,9 +15,27 @@ function App() {
 
   useEffect(() => {
     if (web3) {
-      // See https://web3js.readthedocs.io/en/v1.3.4/ for API docs
-      console.log("Web3 object:", web3);
-      web3.eth.getAccounts().then(console.log);
+      async function asyncUseEffect() {
+        // See https://web3js.readthedocs.io/en/v1.3.4/ for API docs
+        console.log("Web3 object:", web3);
+        web3.eth.getAccounts().then(setAccounts);
+
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = ProjectsContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          ProjectsContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+
+        // const result = await instance.methods.addProject(1, "Project 1").send({
+        //   from: "0xC404F3c136076dEc198FF22e374AD049fD55eF3F",
+        //   value: "1000000000000000000",
+        //   gas: 6721974,
+        // });
+        console.log(await instance.methods.projectCount().call());
+        // console.log(result);
+      }
+      asyncUseEffect();
     }
   }, [web3]);
 
